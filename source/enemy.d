9f30d4474a4feb7e8f90041ec2d861abd25ec4a0
Jonath KANE <jonathkane@gmail.com>
2025-10-25 14:23:22 +0200

    Marisa turret

--- source/enemy.d
+++ source/enemy.d
@@ -19,16 +19,21 @@ final class EnemyController : Controller!Actor {
 final class EnemyBehavior : Behavior!Actor {
     private {
         bool _shot = false;
+        int _life;
         string _enemyId;
         GrTask _task;
     }
 
-    this(string enemyId) {
+    this(string enemyId, int life = 5) {
         _enemyId = enemyId;
+        _life = life;
     }
 
     override void onStart() {
         _task = Atelier.script.callEvent(_enemyId ~ "Behavior", [grGetNativeType("Actor")], [GrValue(entity)]);
+    }
+
+    override void onHit(Vec3f normal) {
     }
 
     override void update() {
