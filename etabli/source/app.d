2d0acb4dfc95b4d4204f6145f87b9f49a510e01f
Enalye <enalyen@gmail.com>
2025-12-22 23:03:04 +0100

    Config taille fenêtre établi

--- etabli/source/app.d
+++ etabli/source/app.d
@@ -1,6 +1,8 @@
 import atelier;
 import thj;
 
+import std.file : exists;
+
 extern (C) __gshared string[] rt_options = [
     "gcopt=initReserve:128 minPoolSize:256 parallel:2"
 ];
@@ -9,7 +11,15 @@ void main(string[] args) {
     Atelier.openLogger(false);
 
     try {
-        Atelier atelier = new Atelier(1920, 1080, "Établi", &setupResourceLoaders, &setupLibLoaders);
+        Vec2u windowSize = Vec2u(1920, 1080);
+        if (exists("etabli.ffd")) {
+            Farfadet ffd = Farfadet.fromFile("etabli.ffd");
+            if (ffd.hasNode("windowSize")) {
+                windowSize = ffd.getNode("windowSize").get!Vec2u(0);
+            }
+        }
+
+        Atelier atelier = new Atelier(windowSize.x, windowSize.y, "Établi", &setupResourceLoaders, &setupLibLoaders);
 
         atelier.etabli.open();
         atelier.run();
